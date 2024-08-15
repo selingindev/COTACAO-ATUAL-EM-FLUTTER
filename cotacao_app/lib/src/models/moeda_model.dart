456d77a9c598bc13db9b6c1a23515788128a83e1
Arthur Selingin <arthur_santos@outlook.com.br>
2024-08-15 08:47:57 -0300

    Instalando depedencias e quase fim da model

--- cotacao_app/lib/src/models/moeda_model.dart
+++ cotacao_app/lib/src/models/moeda_model.dart
@@ -1,6 +1,15 @@
-class MoedaModel {
+import 'package:equatable/equatable.dart';
+
+class MoedaModel extends Equatable {
   final String name;
   final String code;
 
   MoedaModel({required this.name, required this.code});
+
+  factory MoedaModel.init() {
+    return MoedaModel(name: '', code: '');
+  }
+
+  @override
+  List<Object> get props => [name, code];
 }
